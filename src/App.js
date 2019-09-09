7b942e1075bb881303293bd077e8ab90ce4ebe46
Mike Plis <michael.plis01@gmail.com>
2019-09-09 07:46:03 -0400

    WIP calculator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,217 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const OPERATORS = {
+    DIV: 'div',
+    MULT: 'mult',
+    ADD: 'add',
+    SUB: 'sub'
+};
+
+function Item(props) {
+    return <button {...props} />;
+}
+
+function computeValue(leftOperand, rightOperand, operator) {
+    switch (operator) {
+        case OPERATORS.ADD: {
+            return leftOperand + rightOperand;
+        }
+        case OPERATORS.SUB: {
+            return leftOperand - rightOperand;
+        }
+        case OPERATORS.MULT: {
+            return leftOperand * rightOperand;
+        }
+        case OPERATORS.DIV: {
+            return leftOperand / rightOperand;
+        }
+        default:
+            break;
+    }
+}
+
+// const initialState = {
+//     operator: null,
+//     currentOperand: 0,
+//     previousOperand: null,
+//     waitingForOperand: true
+// };
+//
+// function calculatorReducer(state, action) {
+//     switch (action.type) {
+//         case 'SELECT_DIGIT': {
+//             const { currentOperand } = state;
+//             const operandAsString = currentOperand ? currentOperand.toString() : '';
+//             return {
+//                 ...state,
+//                 currentOperand: Number(operandAsString + action.digit)
+//             };
+//         }
+//         case 'SET_OPERAND': {
+//             return {
+//                 ...state,
+//                 currentOperand: action.value
+//             };
+//         }
+//         case 'SELECT_OPERATOR': {
+//             const { currentOperand, previousOperand, operator } = state;
+//             return {
+//                 ...state,
+//                 operator: action.operator,
+//                 currentOperand: previousOperand
+//                     ? computeValue(previousOperand, currentOperand, operator)
+//                     : null,
+//                 previousOperand: currentOperand
+//             };
+//         }
+//         case 'COMPUTE_VALUE': {
+//             const { currentOperand, previousOperand, operator } = state;
+//             return {
+//                 ...state,
+//                 currentOperand: computeValue(previousOperand, currentOperand, operator),
+//                 previousOperand: null
+//             };
+//         }
+//         case 'CLEAR_DIGIT': {
+//             const operandAsString = state.currentOperand ? state.currentOperand.toString() : '';
+//             return {
+//                 ...state,
+//                 currentOperand: Number(operandAsString.slice(0, operandAsString.length - 1))
+//             };
+//         }
+//         case 'CLEAR_ALL': {
+//             return initialState;
+//         }
+//         default:
+//             break;
+//     }
+// }
+
+const initialState = {
+    operator: null,
+    value: 0,
+    displayValue: '0',
+    waitingForOperand: true
+};
+
+function calculatorReducer(state, action) {
+    switch (action.type) {
+        case 'SELECT_DIGIT': {
+            const { displayValue } = state;
+            return {
+                ...state,
+                displayValue: Number(displayValue + action.digit).toString()
+            };
+        }
+        case 'SET_OPERAND': {
+            return {
+                ...state,
+                displayValue: action.value
+            };
+        }
+        case 'SELECT_OPERATOR': {
+            const { value, displayValue, operator, waitingForOperand } = state;
+            if (waitingForOperand) {
+                return {
+                    ...state,
+                    operator: action.operator
+                };
+            }
+            const newValue = computeValue(value, Number(displayValue), operator);
+            return {
+                ...state,
+                operator: action.operator,
+                value: newValue
+            };
+        }
+        case 'COMPUTE_VALUE': {
+            const { value, displayValue, operator } = state;
+            return {
+                ...state,
+                value: computeValue(value, Number(displayValue), operator)
+            };
+        }
+        case 'CLEAR_DIGIT': {
+            const { displayValue } = state;
+            return {
+                ...state,
+                displayValue: displayValue.slice(0, displayValue - 1)
+            };
+        }
+        case 'CLEAR_ALL': {
+            return initialState;
+        }
+        default:
+            break;
+    }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, dispatch] = React.useReducer(calculatorReducer, initialState);
+
+    const { displayValue } = state;
+
+    return (
+        <div>
+            <input
+                value={displayValue}
+                onChange={e => {
+                    const value = Number(e.target.value);
+                    if (value) {
+                        dispatch({ type: 'SET_OPERAND', value });
+                    }
+                }}
+            />
+            <div style={{ display: 'flex' }}>
+                <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)' }}>
+                    <Item onClick={() => dispatch({ type: 'SELECT_DIGIT', digit: '7' })}>7</Item>
+                    <Item onClick={() => dispatch({ type: 'SELECT_DIGIT', digit: '8' })}>8</Item>
+                    <Item onClick={() => dispatch({ type: 'SELECT_DIGIT', digit: '9' })}>9</Item>
+                    <Item onClick={() => dispatch({ type: 'SELECT_DIGIT', digit: '4' })}>4</Item>
+                    <Item onClick={() => dispatch({ type: 'SELECT_DIGIT', digit: '5' })}>5</Item>
+                    <Item onClick={() => dispatch({ type: 'SELECT_DIGIT', digit: '6' })}>6</Item>
+                    <Item onClick={() => dispatch({ type: 'SELECT_DIGIT', digit: '1' })}>1</Item>
+                    <Item onClick={() => dispatch({ type: 'SELECT_DIGIT', digit: '2' })}>2</Item>
+                    <Item onClick={() => dispatch({ type: 'SELECT_DIGIT', digit: '3' })}>3</Item>
+                    <Item onClick={() => dispatch({ type: 'SELECT_DIGIT', digit: '0' })}>0</Item>
+                    <Item>.</Item>
+                    <Item onClick={() => dispatch({ type: 'COMPUTE_VALUE' })}>=</Item>
+                </div>
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                    <Item onClick={() => dispatch({ type: 'CLEAR_DIGIT' })}>C</Item>
+                    <Item onClick={() => dispatch({ type: 'CLEAR_ALL' })}>AC</Item>
+                    <Item
+                        onClick={() =>
+                            dispatch({ type: 'SELECT_OPERATOR', operator: OPERATORS.DIV })
+                        }
+                    >
+                        /
+                    </Item>
+                    <Item
+                        onClick={() =>
+                            dispatch({ type: 'SELECT_OPERATOR', operator: OPERATORS.MULT })
+                        }
+                    >
+                        x
+                    </Item>
+                    <Item
+                        onClick={() =>
+                            dispatch({ type: 'SELECT_OPERATOR', operator: OPERATORS.SUB })
+                        }
+                    >
+                        -
+                    </Item>
+                    <Item
+                        onClick={() =>
+                            dispatch({ type: 'SELECT_OPERATOR', operator: OPERATORS.ADD })
+                        }
+                    >
+                        +
+                    </Item>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
